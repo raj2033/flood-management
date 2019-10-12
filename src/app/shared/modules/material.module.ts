7c771d736e46736ebe51710106f3d2743447a6c5
Shivani Jain <shivani@Shivanis-MacBook-Air.local>
2019-10-12 23:51:29 +0530

    SideNavBar and area header

--- src/app/shared/modules/material.module.ts
+++ src/app/shared/modules/material.module.ts
@@ -1,11 +1,33 @@
+import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 import { NgModule } from '@angular/core';
 
 // Material Components
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSelectModule } from '@angular/material/select';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
-const MatModules = [MatCardModule, MatButtonModule, MatSelectModule];
+const MatModules = [
+  MatCardModule,
+  MatButtonModule,
+  MatSelectModule,
+  MatAutocompleteModule,
+  MatSidenavModule,
+  MatIconModule,
+  MatToolbarModule,
+  MatDividerModule,
+  MatListModule,
+  MatMenuModule,
+  MatInputModule,
+  FlexLayoutModule
+];
 
 @NgModule({
   imports: [MatModules],
