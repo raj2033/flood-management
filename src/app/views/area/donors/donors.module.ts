f28a64daca8a5bbdd2a909549adf16c3ed25dc7e
Raj Sekhar <krj2033@gmail.com>
2019-10-13 03:59:23 +0530

    add donors list

--- src/app/views/area/donors/donors.module.ts
+++ src/app/views/area/donors/donors.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { DonorsRoutingModule } from './donors-routing.module';
 import { DonorsComponent } from './donors.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   declarations: [DonorsComponent],
-  imports: [CommonModule, DonorsRoutingModule]
+  imports: [CommonModule, DonorsRoutingModule, SharedModule]
 })
 export class DonorsModule {}
