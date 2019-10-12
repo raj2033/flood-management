d35988fd821729ce378ec8da7c44ac166ab834e3
Raj Sekhar <krj2033@gmail.com>
2019-10-13 01:24:33 +0530

    add home page

--- src/app/views/homepage/homepage.module.ts
+++ src/app/views/homepage/homepage.module.ts
@@ -3,13 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { HomepageRoutingModule } from './homepage-routing.module';
 import { HomepageComponent } from './homepage.component';
-
+import { SharedModule } from '../../shared/shared.module';
 
 @NgModule({
   declarations: [HomepageComponent],
-  imports: [
-    CommonModule,
-    HomepageRoutingModule
-  ]
+  imports: [CommonModule, HomepageRoutingModule, SharedModule]
 })
-export class HomepageModule { }
+export class HomepageModule {}
