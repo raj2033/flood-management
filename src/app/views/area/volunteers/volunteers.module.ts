2909fdbfd07293360572534fce4a1c4f4b8b58e5
Raj Sekhar <krj2033@gmail.com>
2019-10-13 06:15:42 +0530

    add volunteers view

--- src/app/views/area/volunteers/volunteers.module.ts
+++ src/app/views/area/volunteers/volunteers.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { VolunteersRoutingModule } from './volunteers-routing.module';
-
+import { VolunteersComponent } from './volunteers.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    VolunteersRoutingModule
-  ]
+  declarations: [VolunteersComponent],
+  imports: [CommonModule, VolunteersRoutingModule, SharedModule]
 })
-export class VolunteersModule { }
+export class VolunteersModule {}
