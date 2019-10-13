d3227381d328d0350819dc0e0a14d5dd9bc40a2f
Shivani Jain <shivani@Shivanis-MacBook-Air.local>
2019-10-13 14:03:48 +0530

    Last changes

--- src/app/views/area/request-help/request-help.component.ts
+++ src/app/views/area/request-help/request-help.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { IAreaAction } from 'src/app/models/area.model';
+import { AreaService } from 'src/app/services/area.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-request-help',
@@ -6,10 +9,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./request-help.component.scss']
 })
 export class RequestHelpComponent implements OnInit {
-
-  constructor() { }
+  dataSource: IAreaAction[] = [];
+  displayedColumns: string[] = ['Name', 'Contact', 'Help'];
+  constructor(private areaService: AreaService, private route: ActivatedRoute) {}
 
   ngOnInit() {
+    const areaId = parseInt(window.location.href.split('/')[4], 10);
+    this.areaService
+      .getRequests(areaId)
+      .subscribe((data: IAreaAction[]) => (this.dataSource = data));
   }
-
 }
