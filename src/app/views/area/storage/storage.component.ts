4d696ca74707452b8edd6318d9b823c8d030558b
Raj Sekhar <krj2033@gmail.com>
2019-10-13 12:30:10 +0530

    fix: update data in area page on changing location

--- src/app/views/area/storage/storage.component.ts
+++ src/app/views/area/storage/storage.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { IStorage, IStorageList } from 'src/app/models/area.model';
 import { AreaService } from 'src/app/services/area.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-storage',
@@ -12,10 +13,12 @@ export class StorageComponent implements OnInit {
   items: IStorage[] = [];
 
   displayedColumns = ['Item Name', 'Item Quantity'];
-  constructor(private areaService: AreaService) {}
+  constructor(private areaService: AreaService, private route: ActivatedRoute) {}
 
   ngOnInit() {
-    this.areaService.getAllStorages(43).subscribe(data => (this.storages = data));
+    this.route.parent.parent.params.subscribe(param => {
+      this.areaService.getAllStorages(param.areaId).subscribe(data => (this.storages = data));
+    });
   }
 
   planelOpened(storageId: number) {
